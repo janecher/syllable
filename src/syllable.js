13c96d8978d98743159dabd6236bded3486ef216
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-25 15:27:35 -0700

    add functions to generate haiku strings
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>
    co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>

--- src/syllable.js
+++ src/syllable.js
@@ -1,3 +1,5 @@
+const randomWords = require('random-words');
+
 export function Haiku(line1, line2, line3) {
     this.line1 = line1;
     this.line2 = line2;
@@ -60,17 +62,7 @@ export function arrayCycling7(arr){
   }
 }
 
-/*export function syllables(word) {
-  word = word.toLowerCase();
-  if(word.length <= 3) { 
-    return 1; 
-  }
-  return word.replace(/(?:[^laeiouy]es|ed|lle|[^laeiouy]e)$/, '')
-    .replace(/^y/, '')
-    .match(/[aeiouy]{1,2}/g).length;
-} */
-
-/*export function createHaiku5() {
+export function createHaiku5() {
   let arrayOfWords = [];
   let numberOfSyllInArray = 0;
   while(numberOfSyllInArray !== 5) {
@@ -79,7 +71,7 @@ export function arrayCycling7(arr){
       newWord = randomWords(); 
     }
     if ((numberOfSyllInArray + syllables(newWord)) <= 5) {
-      arrayOfWords.push(newWords);
+      arrayOfWords.push(newWord);
       numberOfSyllInArray += syllables(newWord);
     }       
   }
@@ -89,4 +81,25 @@ export function arrayCycling7(arr){
   }
   haiku += arrayOfWords[arrayOfWords.length -1];
   return haiku;
-}*/
+}
+
+export function createHaiku7() {
+  let arrayOfWords = [];
+  let numberOfSyllInArray = 0;
+  while(numberOfSyllInArray !== 7) {
+    let newWord = randomWords(); 
+    while (syllables(newWord) > 7) {
+      newWord = randomWords(); 
+    }
+    if ((numberOfSyllInArray + syllables(newWord)) <= 7) {
+      arrayOfWords.push(newWord);
+      numberOfSyllInArray += syllables(newWord);
+    }       
+  }
+  let haiku = "";
+  for(let i = 0; i< arrayOfWords.length -1; i++) {
+    haiku += arrayOfWords[i] + " ";
+  }
+  haiku += arrayOfWords[arrayOfWords.length -1];
+  return haiku;
+}
